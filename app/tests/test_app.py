5464f3a08ac0dcb4a8c5e1122d3e3f3dfde90dda
chaymae <chaymaeamador14@gmail.com>
2025-10-05 17:06:05 +0100

    initinal commit of flask app with docker and k8s setup

---
+++ app/tests/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['message'] == "Hello World!"
